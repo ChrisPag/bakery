48de3da61a573f7118ffeec80ff7c741de5c8b11
ChrisPag <chrisp99@my.yorku.ca>
2021-11-19 14:12:09 -0500

    initial commit

---
+++ src/Navbar.js
@@ -0,0 +1,11 @@
+import CupcakeIcon from './img/cupcake-icon.png';
+
+const Navbar = () => {
+    return(
+        <div className="navbar">
+            <img src={CupcakeIcon} alt="cupcake icon"></img>
+        </div>
+    );
+}
+
+export default Navbar;
